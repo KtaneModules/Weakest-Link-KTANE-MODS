25f642819ba2a71689de6e21801dfcbe441e8772
Nya <nyabentley@gmail.com>
2023-05-16 21:34:49 -0400

    found a way to load data from json

---
+++ Assets/Weakest Link/Scripts/JsonReader.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonReader : MonoBehaviour {
+
+    private void Start()
+    {
+        LoadData();
+    }
+
+	//object used in order to read data from json
+
+	public TextAsset textJSON;
+
+    private JsonData json;
+
+    //holds the data read from the json
+    [System.Serializable]
+    public class JsonData
+    {
+        List<string> name; //list of all the names of the contestants
+        public List<JsonTrivia> allQuestions; //list of all the questions and its respective data
+    }
+
+    public class JsonTrivia
+    {
+        public string Question; //the question being asked
+        public List<string> AcceptedAnswers;  //strings that will pass the questions
+        public List<string> WrongAnswers;  //strings that the cpu will use the when they need to get the question wrong
+        public string Category;  //They category of the question
+
+        public JsonTrivia(string Question, List<string> AcceptedAnswers, List<string> WrongAnswers, string Category)
+        {
+            this.Question = Question;
+            this.AcceptedAnswers = AcceptedAnswers;
+            this.WrongAnswers = WrongAnswers;
+            this.Category = Category;
+        }
+    }
+
+    //Loads the data from the json
+    public void LoadData()
+    {
+        Debug.Log("Loading data...\n" + textJSON.text);
+        json = JsonUtility.FromJson<JsonData>(textJSON.text);
+
+
+    }
+}
--- Assets/Weakest Link/Scripts/Trivia.cs
+++ Assets/Weakest Link/Scripts/Trivia.cs
@@ -2,22 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Category
+{
+    Geography,
+    Language,
+    Wildlife,
+    Biology,
+    Maths,
+    KTANE
+}
+
+
+
 public class Trivia {
 
-    public enum Category
+    public string Question { get; private set; } //the question being asked
+    public List<string> AcceptedAnswers { get; private set; } //strings that will pass the questions
+    public List<string> WrongAnswers { get; private set; } //strings that the cpu will use the when they need to get the question wrong
+    public Category Category { get; private set; } //They category of the question
+
+
+    public Trivia(string question, List<string> acceptedAnswers, List<string> wrongAnswers, Category category)
     {
-        Geography,
-        Language,
-        Wildlife,
-        Biology,
-        Maths,
-        KTANE
+        this.Question = question;
+        this.AcceptedAnswers = acceptedAnswers;
+        this.WrongAnswers = wrongAnswers;
+        this.Category = category;
     }
-    
-    public Trivia(string question, List<string> acceptedAnswers, List<string> wrongAnswers)
-    {
-
-    }
-
-
 }
